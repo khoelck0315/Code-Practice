e3ace000cd944eaadb4074ceba72a555d485a366
Kevin Hoelck <kevin.hoelck@nlcinsurance.com>
2020-09-13 21:44:13 -0400

    Completed "Range Extraction"

--- rangeextraction.js
+++ rangeextraction.js
@@ -10,10 +10,9 @@ solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20
 
 function solution(list) {
   return list.map((val,ind,arr) => {
-    console.log(Math.abs(val - arr[ind+1]))
-    if(Math.abs(val - arr[ind+1] == 1)) {
+    if((Math.abs(val - arr[ind+2])) == 2) {
       const low = val;
-      while(Math.abs(val - arr[ind+1] == 1)) {
+      while((Math.abs(val++ - arr[ind+1])) == 1) {
         arr.splice(ind,1)
       }
       return `${low}-${arr[ind]}`;
@@ -21,8 +20,9 @@ function solution(list) {
     else {
       return val;
     }
-  }).join(',');
+  }).filter((e) => (e!='')).join(',');
 }
 
 //Testing
 console.log(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]));  
+//Expecting"-6,-3-1,3-5,7-11,14,15,17-20"
